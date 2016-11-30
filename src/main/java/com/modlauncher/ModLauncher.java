b83833448b513e564192e95407fc85c638d656da
Eyeballcode <edwardyeung39@gmail.com>
2016-12-01 01:22:35 +0800

    Recomment

--- src/main/java/com/modlauncher/ModLauncher.java
+++ src/main/java/com/modlauncher/ModLauncher.java
@@ -33,8 +33,8 @@ public class ModLauncher {
         ModPack modPack = ModPackLookup.lookupModpackByName("EyePack");
         System.out.println(modPack.getDescription());
 
-//        modPack.getMCVersion().download();
-//        modPack.getForgeVersion().download();
+        modPack.getMCVersion().download();
+        modPack.getForgeVersion().download();
         modPack.downloadMods();
         modPack.setupFolders();
         modPack.setupMods();
